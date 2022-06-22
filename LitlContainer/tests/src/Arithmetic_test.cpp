d028971aff813f1d809c1a16a0382d1ffa119237
Antoine Basset <antoine.basset@cnes.fr>
2022-06-22 21:02:39 +0200

    DOC: Add general introduction to primer

--- LitlContainer/tests/src/Arithmetic_test.cpp
+++ LitlContainer/tests/src/Arithmetic_test.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2022, Antoine Basset
+// @copyright 2022, Antoine Basset (CNES)
 // This file is part of Raster <github.com/kabasset/Raster>
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
