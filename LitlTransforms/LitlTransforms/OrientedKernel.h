59d7d0d875f54e3d8acfc9b67ea8b3740606f648
Antoine Basset <antoine.basset@cnes.fr>
2022-08-05 10:47:15 +0200

    WIP: Testing OrientedKernel

---
+++ LitlTransforms/LitlTransforms/OrientedKernel.h
@@ -0,0 +1,94 @@
+// @copyright 2022, Antoine Basset (CNES)
+// This file is part of Litl <github.com/kabasset/Raster>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef _LITLTRANSFORMS_ORIENTEDKERNEL_H
+#define _LITLTRANSFORMS_ORIENTEDKERNEL_H
+
+#include "LitlRaster/Raster.h"
+#include "LitlTypes/Slice.h"
+
+#include <type_traits> // decay
+#include <vector>
+
+namespace Litl {
+
+/**
+ * @brief An axis-aligned 1D kernel.
+ */
+template <typename T, Index I>
+class OrientedKernel {
+public:
+  /**
+   * @brief The index of the axis the kernel is aligned to.
+   */
+  static constexpr Index Axis = I;
+
+  /**
+   * @brief Constructor.
+   */
+  OrientedKernel(std::vector<T> values, Index origin) : m_values(std::move(values)), m_origin(origin) {}
+
+  /**
+   * @brief Create a kernel with centered origin.
+   * 
+   * If the number of values is even, then the origin index is rounded down.
+   */
+  OrientedKernel(std::vector<T> values) : OrientedKernel(values, values.size() / 2) {}
+
+  /**
+   * @brief Get the origin index.
+   */
+  Index origin() const {
+    return m_origin;
+  }
+
+  /**
+   * @brief Get the kernel window.
+   */
+  Slice window() const {
+    return Slice::fromSize(-m_origin, m_values.size());
+  }
+
+  /**
+   * @brief Correlate an input extrapolator.
+   */
+  template <typename TExtrapolator>
+  Raster<std::decay_t<typename TExtrapolator::Value>, TExtrapolator::Dimension>
+  operator*(const TExtrapolator& in) const {
+    Raster<std::decay_t<typename TExtrapolator::Value>, TExtrapolator::Dimension> out(in.shape());
+    correlateTo(in, out, out.domain());
+    return out;
+  }
+
+  /**
+   * @brief Correlate an input raster or extrapolator over a specified region.
+   */
+  template <typename TIn, typename TOut, typename TRegion>
+  void correlateTo(const TIn& in, TOut& out, const TRegion region) const {
+    auto front = Position<TIn::Dimension>::zero();
+    front[I] -= m_origin;
+    auto patch = in.subraster(OrientedSlice<I, TIn::Dimension>::fromSize(std::move(front), m_values.size()));
+    for (const auto& p : region) {
+      patch.shift(p); // FIXME patch.moveBy(p)
+      out[p] = std::inner_product(m_values.begin(), m_values.end(), patch.begin(), T {});
+      // FIXME use out.subraster(region) iterator?
+      patch.shiftBack(p); // FIXME patch.moveAt(front);
+    }
+  }
+
+private:
+  /**
+   * @brief The kernel values.
+   */
+  std::vector<T> m_values;
+
+  /**
+   * @brief The origin index.
+   */
+  Index m_origin;
+};
+
+} // namespace Litl
+
+#endif
---
+++ LitlTransforms/tests/src/OrientedKernel_test.cpp
@@ -0,0 +1,38 @@
+// @copyright 2022, Antoine Basset (CNES)
+// This file is part of Litl <github.com/kabasset/Raster>
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include "LitlTransforms/Interpolation.h"
+#include "LitlTransforms/OrientedKernel.h"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace Litl;
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE(OrientedKernel_test)
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(sum3_test) {
+  const auto raster = Raster<int>({3, 3}).range();
+  const auto in = extrapolate(raster, 0);
+  const auto kernel0 = OrientedKernel<int, 0>({1, 1, 1});
+  const auto kernel1 = OrientedKernel<int, 1>({1, 1, 1});
+  const auto out0 = kernel0 * in;
+  const auto out1 = kernel1 * in;
+  // 0 1 2 //  1  3  3 // 3  5  7
+  // 3 4 5 //  7 12  9 // 9 12 15
+  // 6 7 8 // 13 21 15 // 9 11 13
+  const std::vector<int> expected0 {1, 3, 3, 7, 12, 9, 13, 21, 15};
+  const std::vector<int> expected1 {3, 5, 7, 9, 12, 15, 9, 11, 13};
+  BOOST_TEST(out0.shape() == raster.shape());
+  BOOST_TEST(out0.container() == expected0);
+  BOOST_TEST(out1.shape() == raster.shape());
+  BOOST_TEST(out1.container() == expected1);
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_SUITE_END()
