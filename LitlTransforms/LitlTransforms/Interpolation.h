c49a17cf3456da7b72541b408b706a6f6b4e00c9
Antoine Basset <antoine.basset@cnes.fr>
2022-10-05 15:46:03 +0200

    Rename raster as parent in interpolator

--- LitlTransforms/LitlTransforms/Interpolation.h
+++ LitlTransforms/LitlTransforms/Interpolation.h
@@ -13,25 +13,26 @@ namespace Litl {
 /**
  * @ingroup interpolation
  * @brief Interpolation decorator with optional extrapolator.
- * @tparam TRaster The decorated raster or extrapolator type
+ * @tparam TParent The decorated raster or extrapolator type
  * @tparam TMethod The interpolation method
  * 
- * This class provides a subscript operator which accepts a position,
- * which can lie between pixels or outside the raster domain.
+ * This class provides a function call operator which accepts a non-integral position.
+ * If `TParent` is a raster extrapolator, then the position can be outside the raster domain.
  * 
  * The interpolation formula is implemented as `TMethod::at()`,
- * while the extrapolation formula is implemented as `TRaster::operator[]()`.
- * If `TRaster` is a raster, then no bound checking is performed.
- * This is the best option when no value outside the raster domain has to be evaluated.
+ * while the extrapolation formula (if any) is implemented as `TParent::operator[]()`.
+ * 
+ * If `TParent` is a raster, then no bound checking is performed.
+ * This is the fastest option when no value outside the raster domain has to be evaluated.
  */
-template <typename TRaster, typename TMethod>
+template <typename TParent, typename TMethod>
 class Interpolator {
 
 public:
   /**
    * @brief The input value type.
    */
-  using Value = typename TRaster::Value;
+  using Value = typename TParent::Value;
 
   /**
    * @brief The interpolation value type.
@@ -41,19 +42,26 @@ public:
   /**
    * @brief The interpolation dimension parameter.
    */
-  static constexpr Index Dimension = TRaster::Dimension;
+  static constexpr Index Dimension = TParent::Dimension;
 
   /**
    * @brief Constructor.
    */
-  explicit Interpolator(const TRaster& raster, TMethod&& method = TMethod()) :
-      m_raster(raster), m_method(std::move(method)) {}
+  explicit Interpolator(const TParent& parent, TMethod&& method = TMethod()) :
+      m_parent(parent), m_method(std::move(method)) {}
+
+  /**
+   * @brief Get the decorated parent.
+   */
+  const TParent& parent() const {
+    return m_parent;
+  }
 
   /**
    * @brief Get the decorated raster.
    */
-  auto raster() const -> decltype(auto) {
-    return Litl::rasterize(m_raster);
+  auto raster() const -> decltype(auto) { // FIXME As free function?
+    return Litl::rasterize(m_parent);
   }
 
   /**
@@ -67,35 +75,35 @@ public:
    * @brief Get the raster shape.
    */
   const Position<Dimension>& shape() const {
-    return m_raster.shape();
+    return m_parent.shape();
   }
 
   /**
    * @brief Get the raster domain.
    */
   Box<Dimension> domain() const {
-    return m_raster.domain();
+    return m_parent.domain();
   }
 
   /**
    * @brief Get the value at given integral position.
    */
   inline const Value& operator[](const Position<Dimension>& position) const {
-    return m_raster[position];
+    return m_parent[position];
   }
 
   /**
    * @brief Compute the interpolated value at given position.
    */
   inline Floating operator()(const Vector<double, Dimension>& position) const {
-    return m_method.template at<Floating>(m_raster, position);
+    return m_method.template at<Floating>(m_parent, position);
   }
 
 private:
   /**
    * @brief The raster or extrapolator.
    */
-  const TRaster& m_raster;
+  const TParent& m_parent;
 
   /**
    * @brief The interpolation method.
@@ -107,9 +115,9 @@ private:
  * @relates Interpolator
  * @brief Make an interpolator with given interpolation method.
  */
-template <typename TMethod = NearestNeighbor, typename TRaster, typename... TArgs>
-auto interpolate(const TRaster& raster, TArgs&&... args) -> decltype(auto) {
-  return Interpolator<TRaster, TMethod>(raster, TMethod(std::forward<TArgs>(args)...));
+template <typename TMethod = NearestNeighbor, typename TParent, typename... TArgs>
+auto interpolate(const TParent& parent, TArgs&&... args) -> decltype(auto) {
+  return Interpolator<TParent, TMethod>(parent, TMethod(std::forward<TArgs>(args)...));
 }
 
 } // namespace Litl
