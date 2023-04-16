2cefbc6a91b5de234af2f3b9bf7fb95c30074eee
Antoine Basset <antoine.basser@cnes.fr>
2023-04-16 09:47:06 +0000

    Add Scipy convolution benchmark

---
+++ LinxRun/python/LinxRun/LinxBenchmarkPythonConvolution.py
@@ -0,0 +1,36 @@
+import argparse
+import numpy as np
+from scipy import ndimage
+import time
+
+import ElementsKernel.Logging as log
+
+
+def defineSpecificProgramOptions():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', type=int, default=2048)
+    parser.add_argument('--kernel', type=int, default=5)
+    return parser
+
+
+def mainMethod(args):
+
+    logger = log.getLogger('LinxBenchmarkPythonConvolution')
+
+    image_diameter = args.image
+    kernel_diameter = args.kernel
+    image_shape = (image_diameter, image_diameter)
+    kernel_shape = (kernel_diameter, kernel_diameter)
+
+    logger.info('Generating raster and kernel...')
+    image = np.arange(np.prod(image_shape), dtype=np.float32).reshape(image_shape)
+    kernel = np.arange(np.prod(kernel_shape), dtype=np.float32).reshape(kernel_shape)
+    logger.debug(f'  input: {image}')
+
+    logger.info('Filtering...')
+    start = time.time()
+    ndimage.convolve(image, kernel, output=image, mode='constant', cval=0)
+    end = time.time()
+    logger.debug(f'  output: {image}')
+
+    logger.info(f'  Done in: {(end - start) * 1000} ms')
