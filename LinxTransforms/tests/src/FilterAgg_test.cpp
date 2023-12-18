dc3d6ac3dc7c775529efabd9d9ff63ccc8aa0029
Antoine Basset <antoine.basser@cnes.fr>
2023-12-18 15:28:04 +0000

    Fix Laplacian test

--- LinxTransforms/tests/src/FilterAgg_test.cpp
+++ LinxTransforms/tests/src/FilterAgg_test.cpp
@@ -17,13 +17,13 @@ BOOST_AUTO_TEST_SUITE(FilterAgg_test)
 
 BOOST_AUTO_TEST_CASE(laplacian_plus_test)
 {
-  Raster<int> expected({3, 3}, {0, 1, 0, 1, -2, 1, 0, 1, 0});
+  Raster<int> expected({3, 3}, {0, 1, 0, 1, -4, 1, 0, 1, 0});
   BOOST_TEST((laplacian_filter<int, 0, 1>().impulse()) == expected);
 }
 
 BOOST_AUTO_TEST_CASE(laplacian_minus_test)
 {
-  Raster<int> expected({3, 3}, {0, -1, 0, -1, 2, -1, 0, -1, 0});
+  Raster<int> expected({3, 3}, {0, -1, 0, -1, 4, -1, 0, -1, 0});
   BOOST_TEST((laplacian_filter<int, 0, 1>(-1).impulse()) == expected);
 }
 
