bf4969b823d3ad6e575bc6f0b4d2cfaa8e7fc404
Antoine Basset <antoine.basset@cnes.fr>
2024-02-11 19:52:26 +0000

    Remove Elements dependency

--- LinxRun/src/program/LinxBenchmarkBuffers.cpp
+++ LinxRun/src/program/LinxBenchmarkBuffers.cpp
@@ -2,7 +2,6 @@
 // This file is part of Linx <github.com/kabasset/Linx>
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-#include "ElementsKernel/ProgramHeaders.h"
 #include "Linx/Base/AlignedBuffer.h"
 #include "Linx/Run/Chronometer.h"
 #include "Linx/Run/ProgramOptions.h"
@@ -11,77 +10,64 @@
 #include <string>
 #include <vector>
 
-Elements::Logging logger = Elements::Logging::getLogger("LinxBenchmarkBuffers");
-
 using Chrono = Linx::Chronometer<std::chrono::milliseconds>;
 
 Chrono::Unit benchmark_buffer(Linx::Index size, Linx::Index alignment)
 {
   Chrono chrono;
-  logger.info("Assignment...");
+  std::cout << "Assignment..." << std::endl;
   chrono.start();
   Linx::AlignedBuffer<long> buffer(size, nullptr, alignment);
   auto duration = chrono.stop();
-  logger.info(std::to_string(duration.count()) + "ms");
+  std::cout << "  Done in " << duration.count() << "ms" << std::endl; // FIXME cout << duration << endl?
   long sum = 0;
-  logger.info("Iteration...");
+  std::cout << "Iteration..." << std::endl;
   chrono.start();
   for (const auto& v : buffer) {
     sum += v + 1;
   }
   duration = chrono.stop();
-  logger.info(std::to_string(sum));
-  logger.info(std::to_string(duration.count()) + "ms");
+  std::cout << "  Sum:" << sum << std::endl;
+  std::cout << "  Done in " << duration.count() << "ms" << std::endl;
   return chrono.elapsed();
 }
 
 Chrono::Unit benchmark_vector(Linx::Index size)
 {
   Chrono chrono;
-  logger.info("Initialization...");
+  std::cout << "Initialization..." << std::endl;
   chrono.start();
   std::vector<long> buffer(size);
   auto duration = chrono.stop();
-  logger.info(std::to_string(duration.count()) + "ms");
+  std::cout << "  Done in " << duration.count() << "ms" << std::endl;
   long sum = 0;
-  logger.info("Iteration...");
+  std::cout << "Iteration..." << std::endl;
   chrono.start();
   for (const auto& v : buffer) {
     sum += v + 1;
   }
   duration = chrono.stop();
-  logger.info(std::to_string(sum));
-  logger.info(std::to_string(duration.count()) + "ms");
+  std::cout << "  Sum:" << sum << std::endl;
+  std::cout << "  Done in " << duration.count() << "ms" << std::endl;
   return chrono.elapsed();
 }
 
-class LinxBenchmarkIteration : public Elements::Program {
-public:
+int main(int argc, char const* argv[])
+{
+  Linx::ProgramOptions options;
+  options.named<long>("align", "Alignment for an AlignedBuffer or 0 for a std::vector");
+  options.named<long>("size", "Number of elements", 1000000);
+  options.parse(argc, argv);
+  const auto alignment = options.as<long>("align");
+  const auto size = options.as<long>("size");
 
-  std::pair<OptionsDescription, PositionalOptionsDescription> defineProgramArguments() override
-  {
-    Linx::ProgramOptions options;
-    options.named<long>("align", "Alignment for an AlignedBuffer or 0 for a std::vector");
-    options.named<long>("size", "Number of elements", 1000000);
-    return options.as_pair();
+  if (alignment > 0) {
+    const auto duration = benchmark_buffer(size, alignment);
+    std::cout << "Done in " << duration.count() << "ms" << std::endl;
+  } else {
+    const auto duration = benchmark_vector(size);
+    std::cout << "Done in " << duration.count() << "ms" << std::endl;
   }
 
-  ExitCode mainMethod(std::map<std::string, VariableValue>& args) override
-  {
-    const auto alignment = args["align"].as<long>();
-    const auto size = args["size"].as<long>();
-
-    if (alignment > 0) {
-      const auto duration = benchmark_buffer(size, alignment);
-      logger.info() << "Done in " << duration.count() << "ms";
-    } else {
-      const auto duration = benchmark_vector(size);
-      logger.info() << "Done in " << duration.count() << "ms";
-      logger.info() << "Done in " << duration.count() << "ms";
-    }
-
-    return Elements::ExitCode::OK;
-  }
-};
-
-MAIN_FOR(LinxBenchmarkIteration)
+  return 0;
+}
