1f41ffc8a01aaa5c991139798b148ba037efbf3b
Antoine Basset <antoine.basset@cnes.fr>
2024-11-06 20:19:23 +0100

    WIP: Refactoring filters

--- include/Linx/Transforms/mixins/FilterMixin.h
+++ include/Linx/Transforms/mixins/FilterMixin.h
@@ -12,6 +12,128 @@
 
 namespace Linx {
 
+template <typename T>
+class OffsetBasedIterator {
+public:
+
+  using iterator_category = std::random_access_iterator_tag;
+  using difference_type = int;
+  using value_type = T;
+  using pointer = T*;
+  using reference = T&;
+
+  /**
+   * @brief Constructor.
+   * 
+   * @param data The reference data pointer
+   * @param index The current index
+   * @param args Arguments forwarded to the offsets sequence constructor
+   */
+  KOKKOS_INLINE_FUNCTION explicit OffsetBasedIterator(T* data, std::size_t index, auto&&... args) :
+      m_data(data),
+      m_index(index),
+      m_offsets(LINX_FORWARD(args)...)
+  {}
+
+  KOKKOS_INLINE_FUNCTION static OffsetBasedIterator begin(T* data, auto&&... args)
+  {
+    return OffsetBasedIterator(data, 0, LINX_FORWARD(args)...);
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator end() const
+  {
+    return OffsetBasedIterator(m_data, m_offsets.size());
+  }
+
+  KOKKOS_INLINE_FUNCTION reference operator[](int i) const
+  {
+    return m_data[m_offsets[i]];
+  }
+
+  KOKKOS_INLINE_FUNCTION reference operator*() const
+  {
+    return m_data[m_offsets[m_index]];
+  }
+
+  KOKKOS_INLINE_FUNCTION pointer operator->() const
+  {
+    return m_data + m_offsets[m_index];
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator& operator++()
+  {
+    ++m_index;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator operator++(int)
+  {
+    auto out = *this;
+    ++(*this);
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator& operator+=(int i)
+  {
+    m_index += i;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator operator+(int i) const
+  {
+    auto out = *this;
+    out.m_index += i;
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator& operator--()
+  {
+    --m_index;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator operator--(int)
+  {
+    auto out = *this;
+    --(*this);
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator& operator-=(int i)
+  {
+    m_index -= i;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION OffsetBasedIterator operator-(int i) const
+  {
+    auto out = *this;
+    out.m_index -= i;
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION difference_type operator-(const OffsetBasedIterator& rhs) const
+  {
+    return m_index - rhs.m_index;
+  }
+
+  KOKKOS_INLINE_FUNCTION bool operator==(const OffsetBasedIterator& rhs) const
+  {
+    return m_index == rhs.m_index;
+  }
+
+  KOKKOS_INLINE_FUNCTION bool operator!=(const OffsetBasedIterator& rhs) const
+  {
+    return m_index != rhs.m_index;
+  }
+
+private:
+
+  T* m_data;
+  std::size_t m_index; // FIXME iterator on offsets?
+  Sequence<std::ptrdiff_t, -1> m_offsets; // FIXME -1 by default
+};
+
 /**
  * @brief Filtering task mixin.
  * 
@@ -50,23 +172,103 @@ namespace Linx {
  * auto [extrapolated] = P::Run("opening") | in | Linx::Erode(radius).pad() * Linx::Dilate(radius).pad();
  * \endcode
  */
-template <typename TFootprint, typename TPolicy, typename TDerived>
-class FilterMixin {
+template <typename TFootprint, typename TDerived>
+class SpatialFilterMixin {
 public:
 
-  FilterMixin(TFootprint&& footprint, TPolicy&& policy) :
-      m_footprint(LINX_FORWARD(footprint)), m_policy(LINX_FORWARD(policy))
-  {}
+  SpatialFilterMixin(TFootprint footprint) : m_footprint(LINX_MOVE(footprint)) {}
 
-  auto operator()(const auto& in)
+  template <typename TIn>
+  auto operator()(const TIn& in) const
   {
-    return Filtered(LINX_CRTP_CONST_DERIVED, in);
+    return typename TDerived::Apply<TIn>(m_footprint, in);
+  }
+
+  const auto& footprint() const
+  {
+    return m_footprint;
   }
 
 private:
 
   TFootprint m_footprint; ///< The footprint
-  TPolicy m_policy; ///< The border policy
+};
+
+template <typename TFootprint, typename TIn, typename TDerived>
+class ApplySpatialFilterMixin {
+public:
+
+  ApplySpatialFilterMixin(TFootprint footprint, const TIn& in) :
+      m_footprint(LINX_MOVE(footprint)),
+      m_offsets("offsets", m_footprint.size()),
+      m_in(as_readonly(in))
+  {
+    auto offsets_on_host = on_host(m_offsets);
+    auto index = std::make_shared<Index>(0); // Required for copying into for_each
+    auto front = &m_in.front();
+    for_each<Kokkos::Serial>(
+        "compute_offsets()", // FIXME analytic through strides?
+        m_footprint,
+        KOKKOS_LAMBDA(std::integral auto... is) {
+          offsets_on_host[*index] = &m_in(is...) - front;
+          ++(*index);
+        });
+    Linx::copy_to(offsets_on_host, m_offsets); // FIXME offsets_on_host.copy_to(m_offsets)
+  }
+
+  const auto& footprint() const
+  {
+    return m_footprint;
+  }
+
+  auto domain() const
+  {
+    return Box(m_in.domain().start() - m_footprint.start(), m_in.domain().stop() - m_footprint.stop() + 1);
+  }
+
+  auto operator()(std::integral auto... is) const
+  {
+    const typename TIn::value_type* data = &this->m_in(is...);
+    auto begin = OffsetBasedIterator<const typename TIn::value_type>(data, 0, m_offsets);
+    auto end = begin.end();
+    return LINX_CRTP_CONST_DERIVED.reduce(begin, end);
+  }
+
+  void copy_to(const auto& out) const
+  {
+    for_each(
+        "copy_to",
+        domain(),
+        KOKKOS_LAMBDA(auto... is) { out(is...) = (*this)(is...); }); // FIXME make generic
+  }
+
+private:
+
+  TFootprint m_footprint; ///< The footprint
+  Sequence<std::ptrdiff_t, -1> m_offsets; ///< The footprint offsets in the input
+  decltype(as_readonly(std::declval<TIn>())) m_in; ///< The input
+};
+
+template <typename TFootprint>
+class SumFilter : public SpatialFilterMixin<TFootprint, SumFilter<TFootprint>> {
+public:
+
+  SumFilter(TFootprint footprint) : SpatialFilterMixin<TFootprint, SumFilter>(LINX_MOVE(footprint)) {}
+
+  std::string label() const
+  {
+    return "SumFilter";
+  }
+
+  template <typename TIn>
+  struct Apply : public ApplySpatialFilterMixin<TFootprint, TIn, Apply<TIn>> {
+    using ApplySpatialFilterMixin<TFootprint, TIn, Apply>::ApplySpatialFilterMixin;
+
+    KOKKOS_INLINE_FUNCTION auto reduce(auto begin, auto end) const
+    {
+      return std::reduce(LINX_MOVE(begin), LINX_MOVE(end));
+    }
+  };
 };
 
 template <typename TIn, typename TDerived>
@@ -92,7 +294,8 @@ public:
 protected:
 
   MorphologyFilterMixin(const Sequence<std::ptrdiff_t, -1>& offsets, const TIn& in) :
-      m_offsets(offsets), m_in(as_readonly(in))
+      m_offsets(offsets),
+      m_in(as_readonly(in))
   {}
 
   Sequence<std::ptrdiff_t, -1> m_offsets;
