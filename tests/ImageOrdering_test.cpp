e3bab8c274fcca705e7c278dc323723f4a4c9624
Antoine BASSET <antoine.basset@cnes.fr>
2024-11-18 16:25:12 +0000

    Fix ImageOrdering_test on GPU

--- tests/ImageOrdering_test.cpp
+++ tests/ImageOrdering_test.cpp
@@ -8,29 +8,43 @@
 
 #include <boost/test/unit_test.hpp>
 
+template <typename TIn, typename TTest>
+struct CompareValues {
+  TIn m_in;
+  TTest m_test;
+  KOKKOS_INLINE_FUNCTION void operator()(auto... is) const
+  {
+    m_test(is...) = (m_in.front() + m_in.offset(is...) == m_in(is...));
+  }
+};
+
+template <typename TIn, typename TTest>
+struct CompareAddresses {
+  TIn m_in;
+  TTest m_test;
+  KOKKOS_INLINE_FUNCTION void operator()(auto... is) const
+  {
+    m_test(is...) = (&m_in.front() + m_in.offset(is...) == &m_in(is...));
+  }
+};
+
 LINX_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
 BOOST_AUTO_TEST_CASE(offset_test)
 {
-  auto in = Linx::Image<Linx::Index, 6>("in", Linx::Position({1, 2, 3, 4, 5, 6})).fill_with_offsets();
+  auto in = Linx::Image<Linx::Index, 6>("in", 1, 2, 3, 4, 5, 6).fill_with_offsets();
   auto test = Linx::Image<Linx::Index, 6>("test", in.shape());
 
-  Linx::for_each(
-      "test values",
-      in.domain(),
-      KOKKOS_LAMBDA(auto... is) { test(is...) = (in.front() + in.offset(is...) == in(is...)); });
+  Linx::for_each("test values", in.domain(), CompareValues {in, test});
   BOOST_TEST(Linx::sum(test) == test.size());
 
-  Linx::for_each(
-      "test addresses",
-      in.domain(),
-      KOKKOS_LAMBDA(auto... is) { test(is...) = (&in.front() + in.offset(is...) == &in(is...)); });
+  Linx::for_each("test addresses", in.domain(), CompareAddresses {in, test});
   BOOST_TEST(Linx::sum(test) == test.size());
 }
 
 BOOST_AUTO_TEST_CASE(stride_test)
 {
-  auto in = Linx::Image<bool, 8>("in", Linx::Position({1, 2, 3, 4, 5, 6, 7, 8}));
+  auto in = Linx::Image<bool, 8>("in", 1, 2, 3, 4, 5, 6, 7, 8);
   auto strides = in.strides();
   BOOST_TEST(in.offset(8, 0, 0, 0, 0, 0, 0, 0) == 8 * strides[0]);
   BOOST_TEST(in.offset(0, 7, 0, 0, 0, 0, 0, 0) == 7 * strides[1]);
