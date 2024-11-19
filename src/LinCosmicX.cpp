41444eb295d1adcf5ca8d17d50fe46906ded4051
Antoine BASSET <antoine.basset@cnes.fr>
2024-11-19 10:04:07 +0000

    Replace lambdas with functors for NVCC

--- src/LinCosmicX.cpp
+++ src/LinCosmicX.cpp
@@ -31,12 +31,19 @@ Linx::Box<2> strel(Linx::Index radius)
   return {{-radius, -radius}, {radius + 1, radius + 1}};
 }
 
-struct Updatemask {
+struct ScaleNoise {
+  double readnoise2;
+  KOKKOS_INLINE_FUNCTION auto operator()(auto e) const {
+    return std::sqrt(std::max(e, 0.00001) + readnoise2);
+  }
+};
+
+struct UpdateMask {
   double satlevel;
 
   std::string label() const
   {
-    return "Updatemask";
+    return "UpdateMask";
   }
 
   template <typename TData, typename TMask>
@@ -63,10 +70,10 @@ struct Updatemask {
   }
 };
 
-struct Backgroundlevel {
+struct BackgroundLevel {
   std::string label() const
   {
-    return "Backgroundlevel";
+    return "BackgroundLevel";
   }
 
   float operator()(const auto& data, const auto& mask) const // FIXME double?
@@ -78,6 +85,31 @@ struct Backgroundlevel {
       }
     });
     return Linx::StdSort::nth(gooddata, gooddata.size() / 2);
+  }
+};
+
+template <typename T>
+struct FineStructure {
+  KOKKOS_INLINE_FUNCTION auto operator()(auto m_i, auto f_i, auto n_i) const
+  {
+    return std::max<T>(0.01, (f_i - m_i) / n_i);
+  }
+};
+
+struct FindCandidates {
+  double sigclip;
+  double objlim;
+  KOKKOS_INLINE_FUNCTION auto operator()(auto m_i, auto sp_i, auto f_i) const
+  {
+    return (not m_i) && (sp_i > sigclip) && (sp_i / f_i > objlim);
+  }
+};
+
+struct FindNeighborCandidates {
+  double sigclip;
+  KOKKOS_INLINE_FUNCTION auto operator()(auto c_i, auto m_i, auto sp_i) const
+  {
+    return c_i && (not m_i) && (sp_i > sigclip);
   }
 };
 
@@ -103,11 +135,13 @@ std::tuple<TData, Linx::Image<bool, 2>> lacosmicx(
     double psfbeta = 4.765,
     bool verbose = false)
 {
+  using T = typename TData::element_type;
+  
   Linx::TimerLogger logger;
 
   auto [cleanarr] = P::Run("Scale to electrons", logger) | indata | P::Generate(Linx::Add(pssl), Linx::Multiply(gain));
-  auto [mask] = P::Run("Find saturated stars", logger) | P::Input(indata, inmask) | Updatemask(satlevel);
-  auto [backgroundlevel] = P::Run("Get background level", logger) | P::Input(cleanarr, mask) | Backgroundlevel();
+  auto [mask] = P::Run("Find saturated stars", logger) | P::Input(indata, inmask) | UpdateMask(satlevel);
+  auto [backgroundlevel] = P::Run("Get background level", logger) | P::Input(cleanarr, mask) | BackgroundLevel();
 
   const auto sigcliplow = sigfrac * sigclip;
   auto crmask = Linx::Image<bool, 2>("crmask", indata.shape());
@@ -116,30 +150,26 @@ std::tuple<TData, Linx::Image<bool, 2>> lacosmicx(
     logger("Iteration " + std::to_string(i) + " / " + std::to_string(niter));
 
     auto [noise] = P::Run("Compute noise", logger) | cleanarr | Linx::MedianFilter(strel(2))
-        | P::Generate([=](auto e) { return std::sqrt(std::max(e, 0.00001) + readnoise * readnoise); });
+        | P::Generate(ScaleNoise(readnoise * readnoise));
 
     // FIXME keep m5
 
     auto [s] = P::Run("Compute S", logger) | cleanarr
-        | Linx::Upsample(2) /*| Linx::Laplacian<0, 1>(1)*/ | P::Apply(Linx::Max(0.)) // FIXME element_type(0)
-        | Linx::SumFilter(Linx::Box<2>({0, 0}, {2, 2})) // FIXME Lazy SumFilter // FIXME MeanFilter
+        | Linx::Upsample(2) /*| Linx::Laplacian<0, 1>(1)*/ | P::Apply(Linx::Max(T(0)))
+        | Linx::MeanFilter(Linx::Box<2>({0, 0}, {2, 2})) // FIXME Lazy MeanFilter
         | Linx::Downsample(2) | P::Input(noise) | P::Apply(Linx::Divide(), Linx::Divide(2));
 
     auto [sp] = P::Run("Compute S'", logger) | s | Linx::MedianFilter(strel(2)) | P::Input(s)
-        | P::Apply(KOKKOS_LAMBDA(auto m_i, auto s_i) { return s_i - m_i; }); // FIXME negate?
+        | P::Apply(Linx::Subtract()); // FIXME negate!
 
     auto [f_tmp] = P::Run("Compute fine structure", logger) | cleanarr | Linx::Correlation(psfk); // FIXME avoid tmp?
-    auto [f] = P::Run("Compute fine structure", logger) | f_tmp | Linx::MedianFilter(strel(3)) | P::Input(f_tmp, noise)
-        | P::Apply(KOKKOS_LAMBDA(auto m_i, auto f_i, auto n_i) { return std::max(0.01, (f_i - m_i) / n_i); });
+    auto [f] = P::Run("Compute fine structure", logger) | f_tmp | Linx::MedianFilter(strel(3)) //
+        | P::Input(f_tmp, noise) | P::Apply(FineStructure<T>());
 
     auto [cosmics] = P::Run("Find candidate cosmic rays", logger) | P::Input(mask, sp, f)
-        | P::Apply(KOKKOS_LAMBDA(auto m_i, auto sp_i, auto f_i) {
-                       return (not m_i) && (sp_i > sigclip) && (sp_i / f_i > objlim);
-                     })
-        | Linx::Dilation(strel(1)) | P::Input(mask, sp)
-        | P::Apply(KOKKOS_LAMBDA(auto c_i, auto m_i, auto sp_i) { return c_i && (not m_i) && (sp_i > sigclip); })
-        | Linx::Dilation(strel(1)) | P::Input(mask, sp)
-        | P::Apply(KOKKOS_LAMBDA(auto c_i, auto m_i, auto sp_i) { return c_i && (not m_i) && (sp_i) > sigcliplow; });
+        | P::Apply(FindCandidates(sigclip, objlim)) //
+        | Linx::Dilation(strel(1)) | P::Input(mask, sp) | P::Apply(FindNeighborCandidates(sigclip)) //
+        | Linx::Dilation(strel(1)) | P::Input(mask, sp) | P::Apply(FindNeighborCandidates(sigcliplow));
 
     auto numcr = Linx::sum(cosmics);
     logger(std::to_string(numcr) + " cosmic pixels found");
@@ -161,13 +191,10 @@ int main(int argc, char const* argv[])
   context.named("image", "Input length along each axis", 2048);
   context.named("kernel", "Kernel length along each axis", 5);
   context.parse();
-  const auto image_diameter = context.as<int>("image");
-  const auto kernel_diameter = context.as<int>("kernel");
+  const auto extent = context.as<int>("image");
 
-  auto data = Linx::Image<double, 2>("data", image_diameter, image_diameter)
-                  .generate("random noise", Linx::GaussianRng<double>(0, 1));
-  auto mask = Linx::Image<bool, 2>("mask", image_diameter, image_diameter)
-                  .generate("random mask", Linx::UniformRng<int>({0, 2}));
+  auto data = Linx::Image<double, 2>("data", extent, extent).generate("random data", Linx::GaussianRng<double>(0, 1));
+  auto mask = Linx::Image<bool, 2>("mask", extent, extent).generate("random mask", Linx::UniformRng<int>({0, 2}));
 
   print_2d(data);
   print_2d(mask);
