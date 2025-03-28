4c5b95d03c720dea17469dd555271fe45f6c14be
Antoine Basset <antoine.basset@cnes.fr>
2025-03-28 14:31:12 +0100

    Test Laplacian impulse response

--- tests/Laplacian_test.cpp
+++ tests/Laplacian_test.cpp
@@ -18,4 +18,20 @@ BOOST_AUTO_TEST_CASE(box_test)
   BOOST_TEST((box == Linx::Box<2>({-1, -1}, {2, 2})));
 }
 
+BOOST_AUTO_TEST_CASE(impulse_test)
+{
+  const auto in = Linx::Image<int, 3>("in", 5, 5, 5);
+  in(2, 2, 2) = 1;
+  const auto out = Linx::separable_laplacian<0, 1, 2>(-1)(in);
+  const auto expected = Linx::Image<int, 3>("exp", 5, 5, 5);
+  expected(2, 2, 1) = -1;
+  expected(2, 1, 2) = -1;
+  expected(1, 2, 2) = -1;
+  expected(2, 2, 2) = 6;
+  expected(3, 2, 2) = -1;
+  expected(2, 3, 2) = -1;
+  expected(2, 2, 3) = -1;
+  BOOST_TEST((out == expected));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
