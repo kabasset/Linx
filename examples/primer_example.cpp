edfde26fc560df2e2b312929f055f2305e00bc89
Antoine Basset <antoine.basset@cnes.fr>
2025-06-22 18:40:44 +0200

    Add basic snippet to primer

--- examples/primer_example.cpp
+++ examples/primer_example.cpp
@@ -15,6 +15,31 @@
 
 LINX_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
+BOOST_AUTO_TEST_CASE(basics_test)
+{
+  //! [basics]
+  auto a = Linx::Sequence<int, 42>("sequence on device").range(14, 3);
+  const auto a_on_host = Linx::on_host(a);
+  int i = 14;
+  for (auto a_i : a_on_host) {
+    ASSERT(a_i == i);
+    i += 3;
+  }
+
+  auto b = Linx::Image<double, 3>("image on device", 16, 9, 3).fill_with_offsets_from_data();
+  const auto& b_on_host = Linx::on_host(b);
+  ASSERT(b_on_host(0, 0, 0) == 0);
+  ASSERT(b_on_host(15, 8, 2) == b.size() - 1);
+
+  auto c = Linx::Raster<double, 3>("image on host", 16, 9, 3).fill_with_offsets_from_data();
+  i = 0;
+  for (auto c_i : c) {
+    ASSERT(c_i == i);
+    ++i;
+  }
+  //! [basics]
+}
+
 BOOST_AUTO_TEST_CASE(inplace_newinstance_test)
 {
   //! [inplace_newinstance]
