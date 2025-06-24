897419223d5120e0f54b6c296010683e5b96bb0a
Antoine Basset <antoine.basset@cnes.fr>
2025-06-24 19:09:07 +0200

    Fix arrays_example on GPU

--- examples/arrays_example.cpp
+++ examples/arrays_example.cpp
@@ -50,7 +50,7 @@ BOOST_AUTO_TEST_CASE(image_rank_test)
   ASSERT(a.n == 3);
   ASSERT(a.rank() == 3);
   ASSERT(a.size() == 10 * 20 * 30);
-  ASSERT(a.shape() == Linx::Position({10, 20, 30}));
+  ASSERT(a.shape() == Linx::Position<3>({10, 20, 30})); // FIXME NVCC won't deduce N
   ASSERT(a.extent(0) == 10);
   ASSERT(a.extent(1) == 20);
   ASSERT(a.extent(2) == 30);
@@ -77,47 +77,6 @@ BOOST_AUTO_TEST_CASE(image_null_rank_test)
   ASSERT(a.shape() == Linx::Position<0>());
   ASSERT(a.data() == nullptr);
   //! [image_null_rank]
-}
-
-BOOST_AUTO_TEST_CASE(rowwise_test)
-{
-  //! [rowwise]
-  auto a = Linx::rowwise("1D int", {1, 2});
-  auto b = Linx::rowwise("2D char", {{'a', 'b'}, {'c', 'd'}});
-  auto c = Linx::rowwise<float>("3D float", {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}});
-  //! [rowwise]
-
-  static_assert(std::is_same_v<decltype(a)::element_type, int>);
-  ASSERT(a.n == 1);
-  ASSERT(a.label() == "1D int");
-  ASSERT(a.size() == 2);
-  const auto& a_on_host = Linx::on_host(a);
-  ASSERT(a_on_host(0) == 1);
-  ASSERT(a_on_host(1) == 2);
-
-  static_assert(std::is_same_v<decltype(b)::element_type, char>);
-  ASSERT(b.n == 2);
-  ASSERT(b.label() == "2D char");
-  ASSERT(b.size() == 4);
-  const auto& b_on_host = Linx::on_host(b);
-  for (int j = 0; j < 2; ++j) {
-    for (int i = 0; i < 2; ++i) {
-      ASSERT(b_on_host(i, j) == char('a' + 2 * j + i));
-    }
-  }
-
-  static_assert(std::is_same_v<decltype(c)::element_type, float>);
-  ASSERT(c.n == 3);
-  ASSERT(c.label() == "3D float");
-  ASSERT(c.size() == 8);
-  const auto& c_on_host = Linx::on_host(c);
-  for (int k = 0; k < 2; ++k) {
-    for (int j = 0; j < 2; ++j) {
-      for (int i = 0; i < 2; ++i) {
-        ASSERT(c_on_host(i, j, k) == float(1 + 4 * k + 2 * j + i));
-      }
-    }
-  }
 }
 
 BOOST_AUTO_TEST_CASE(access_test)
