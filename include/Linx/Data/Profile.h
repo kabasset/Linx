c267a873b48c5b75114e2317d0add10fc94a76a3
Antoine Basset <antoine.basset@cnes.fr>
2025-07-06 19:55:09 +0200

    DOC: Review Profile doc

--- include/Linx/Data/Profile.h
+++ include/Linx/Data/Profile.h
@@ -14,59 +14,92 @@
 namespace Linx {
 
 /**
- * @brief A mapping between a sequence of 1D indices and a sequence of array elements along a path.
+ * @brief Resizable sequence of elements along a trajectory.
+ * 
+ * Elements are accessed with a 1D index:
+ * the i-th element of the profile is the element of the parent mapping at the i-th position of the trajectory.
+ * For performance, the trajectory is not stored, as opposed to a trajectory-based patch.
+ * The domain of the profile is a 1D slice.
+ * 
+ * Elements can be added up to a given maximum, fixed capacity.
  */
-template <Strided TParent>
+template <Strided TParent> // FIXME must work with all mappings
 class Profile : public DataMixin<typename TParent::value_type, typename TParent::Arithmetic, Profile<TParent>> {
 public:
 
-  using Parent = TParent;
-  static constexpr auto n = TParent::n;
-  using value_type = Parent::value_type;
-  using element_type = std::remove_cvref_t<value_type>;
-  using reference = value_type&;
-  using memory_space = typename TParent::memory_space;
-  using execution_space = typename TParent::execution_space;
+  using Parent = TParent; ///< The parent mapping
+  static constexpr auto n = 1; ///< The rank
+  using value_type = Parent::value_type; ///< The value type
+  using element_type = std::remove_cvref_t<value_type>; ///< The element type
+  using reference = value_type&; ///< The reference type
+  using memory_space = typename TParent::memory_space; ///< The parent memory space
+  using execution_space = typename TParent::execution_space; ///< The parent execution space
 
+  /**
+   * @brief Constructor.
+   * @param parent The parent mapping
+   * @param capacity The maximum size of the sequence
+   */
   Profile(const Parent& parent, std::integral auto capacity) :
       m_parent(parent),
       m_offsets("Profile offsets", capacity),
       m_size("Profile size")
   {}
 
+  /**
+   * @brief Region-based constructor.
+   */
   Profile(const Parent& parent, const NotConvertibleTo<std::size_t> auto& region) : Profile(parent, region.size())
   {
     assign(region);
   }
 
+  /**
+   * @brief Assign the positions of a region.
+   */
   void assign(const auto& region) const
   {
-    for_each<execution_space>(
-        "Profile",
+    for_each<execution_space>( // FIXME keep order?
+        "assign",
         region,
         KOKKOS_CLASS_LAMBDA(std::integral auto... position) { emplace_back(position...); });
   }
 
-  KOKKOS_INLINE_FUNCTION auto rank() const
+  /**
+   * @brief Rank: 1.
+   */
+  static constexpr auto rank() const
   {
-    return m_parent.rank();
+    return 1;
   }
 
+  /**
+   * @brief Number of elements.
+   */
   auto size() const
   {
     return Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), m_size)();
   }
 
+  /**
+   * @brief Index range.
+   */
   auto domain() const
   {
     return Slice(0, size());
   }
 
+  /**
+   * @brief Reference to the i-th element.
+   */
   KOKKOS_INLINE_FUNCTION reference operator()(std::integral auto i) const
   {
     return m_parent.data()[m_offsets(i)]; // FIXME .origin()?
   }
 
+  /**
+   * @brief Reference to the i-th element.
+   */
   KOKKOS_INLINE_FUNCTION reference operator[](std::integral auto i) const
   {
     return m_parent.data()[m_offsets(i)]; // FIXME .origin()?
@@ -86,7 +119,7 @@ public:
    */
   KOKKOS_INLINE_FUNCTION std::ptrdiff_t emplace_back(std::integral auto... position) const
   {
-    auto out = offset_from_origin(m_parent, position...);
+    auto out = offset_from_origin(m_parent, position...); // FIXME no need to require Strided
     push_back(out);
     return out;
   }
@@ -110,7 +143,7 @@ struct FilterOffsets {
   {
     const auto* ptr = &m_in(position...);
     if (m_pred(*ptr)) {
-      m_out.push_back(ptr - m_in.data());
+      m_out.push_back(ptr - m_in.data()); // FIXME .origin()
     }
   }
 };
