ee440e352f1fc70a9c024e58b93fbac1074a9d39
Antoine Basset <antoine.basset@cnes.fr>
2025-07-08 10:07:23 +0200

    Add ProfileIterator

--- include/Linx/Data/Profile.h
+++ include/Linx/Data/Profile.h
@@ -7,18 +7,150 @@
 
 #include "Linx/Base/mixins/Data.h"
 #include "Linx/Base/mixins/Strided.h"
+#include "Linx/Data/Sequence.h"
 
 #include <concepts>
 #include <string>
 
 namespace Linx {
 
+template <typename T>
+class ProfileIterator {
+public:
+
+  using iterator_category = std::random_access_iterator_tag;
+  using difference_type = int;
+  using value_type = T;
+  using pointer = T*;
+  using reference = T&;
+
+  /**
+   * @brief Constructor.
+   * 
+   * @param data The reference data pointer
+   * @param offsets The sequence of address offsets
+   */
+  KOKKOS_INLINE_FUNCTION explicit ProfileIterator(T* data, const std::ptrdiff_t* begin) :
+      m_data(data),
+      m_begin(begin),
+      m_it(begin)
+  {}
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator(const ProfileIterator& rhs) :
+      m_data(rhs.m_data),
+      m_begin(rhs.m_begin),
+      m_it(rhs.m_it)
+  {}
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator& operator=(const ProfileIterator& rhs)
+  {
+    m_data = rhs.m_data;
+    m_begin = rhs.m_begin;
+    m_it = rhs.m_it;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION void reset(T* data)
+  {
+    m_data = data;
+    m_it = m_begin;
+  }
+
+  KOKKOS_INLINE_FUNCTION reference operator[](int i) const
+  {
+    return m_data[m_begin[i]];
+  }
+
+  KOKKOS_INLINE_FUNCTION reference operator*() const
+  {
+    return m_data[*m_it];
+  }
+
+  KOKKOS_INLINE_FUNCTION pointer operator->() const
+  {
+    return m_data + *m_it;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator& operator++()
+  {
+    ++m_it;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator operator++(int)
+  {
+    auto out = *this;
+    ++(*this);
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator& operator+=(int i)
+  {
+    m_it += i;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator operator+(int i) const
+  {
+    auto out = *this;
+    out.m_it += i;
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator& operator--()
+  {
+    --m_it;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator operator--(int)
+  {
+    auto out = *this;
+    --(*this);
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator& operator-=(int i)
+  {
+    m_it -= i;
+    return *this;
+  }
+
+  KOKKOS_INLINE_FUNCTION ProfileIterator operator-(int i) const
+  {
+    auto out = *this;
+    out.m_it -= i;
+    return out;
+  }
+
+  KOKKOS_INLINE_FUNCTION difference_type operator-(const ProfileIterator& rhs) const
+  {
+    return m_it - rhs.m_it;
+  }
+
+  KOKKOS_INLINE_FUNCTION bool operator==(const ProfileIterator& rhs) const
+  {
+    return m_it == rhs.m_it;
+  }
+
+  KOKKOS_INLINE_FUNCTION bool operator!=(const ProfileIterator& rhs) const
+  {
+    return m_it != rhs.m_it;
+  }
+
+private:
+
+  T* m_data; ///< The reference data
+  const std::ptrdiff_t* m_begin; ///< The beginning offset iterator
+  const std::ptrdiff_t* m_it; ///< The current offset iterator
+};
+
 /**
- * @brief Resizable sequence of elements along a trajectory.
+ * @brief Resizable sequence of elements specified as offset from a reference memory address.
  * 
  * Elements are accessed with a 1D index:
- * the i-th element of the profile is the element of the parent mapping at the i-th position of the trajectory.
- * For performance, the trajectory is not stored, as opposed to a trajectory-based patch.
+ * the i-th element of the profile is the i-th inserted element of the parent mapping.
+ * For performance, as opposed to a trajectory-based patch, the positions are not stored.
  * The domain of the profile is a 1D slice.
  * 
  * Elements can be added up to a given maximum, fixed capacity.
@@ -32,6 +164,9 @@ public:
   using value_type = Parent::value_type; ///< The value type
   using element_type = std::remove_cvref_t<value_type>; ///< The element type
   using reference = value_type&; ///< The reference type
+  using pointer = value_type&; ///< The pointer type
+  using iterator = ProfileIterator<value_type>; ///< The iterator type
+  using const_iterator = ProfileIterator<const value_type>; ///< The read-only iterator type
   using memory_space = typename TParent::memory_space; ///< The parent memory space
   using execution_space = typename TParent::execution_space; ///< The parent execution space
 
@@ -87,6 +222,38 @@ public:
   auto domain() const
   {
     return Slice(0, size());
+  }
+
+  /**
+   * @brief Iterator to the beginning.
+   */
+  KOKKOS_INLINE_FUNCTION iterator begin() const
+  {
+    return iterator(m_parent.data(), m_offsets.data());
+  }
+
+  /**
+   * @brief Iterator to the end.
+   */
+  KOKKOS_INLINE_FUNCTION iterator end() const
+  {
+    return iterator(m_parent.data(), m_offsets.data() + m_size());
+  }
+
+  /**
+   * @brief Read-only iterator to the beginning.
+   */
+  KOKKOS_INLINE_FUNCTION const_iterator cbegin() const
+  {
+    return const_iterator(m_parent.data(), m_offsets.data());
+  }
+
+  /**
+   * @brief Read-only iterator to the end.
+   */
+  KOKKOS_INLINE_FUNCTION const_iterator cend() const
+  {
+    return const_iterator(m_parent.data(), m_offsets.data() + m_size());
   }
 
   /**
---
+++ tests/Profile_test.cpp
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: Copyright (C) 2024, Antoine Basset
+// SPDX-License-Identifier: Apache-2.0
+
+#define BOOST_TEST_MODULE ProfileTest
+
+#include "Linx/Data/Image.h"
+#include "Linx/Data/Profile.h"
+#include "Linx/Run/ProgramContext.h"
+
+#include <boost/test/unit_test.hpp>
+
+LINX_AUTO_TEST_SUITE(BOOST_TEST_MODULE) // FIXME to some Testing.h
+
+BOOST_AUTO_TEST_CASE(iteration_test)
+{
+  auto a = Linx::Position<8>("a").arithmetic();
+  auto profile = Linx::Profile(a, 10);
+  for (int i = 0; i < 5; ++i) {
+    profile.push_back(i);
+  }
+
+  BOOST_TEST(profile.size() == 5);
+
+  for (int i = 0; i < 5; ++i) {
+    BOOST_TEST(profile[i] == i);
+  }
+
+  auto it = profile.begin();
+  for (int i = 0; i < 5; ++i) {
+    BOOST_TEST(it[i] == i);
+  }
+
+  int expected = 0;
+  for (const auto& e : profile) {
+    BOOST_TEST(e == expected);
+    ++expected;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(filter_test)
+{
+  auto a = Linx::generate("a", KOKKOS_LAMBDA(int i, int j) { return i + 4 * j; }, 4, 3);
+  auto evens = Linx::filter(a, KOKKOS_LAMBDA(int a_i) { return a_i % 2 == 0; });
+  BOOST_TEST(evens.size() == (a.size() + 1) / 2);
+  BOOST_TEST(Linx::sum(evens) == evens.size() * (evens.size() - 1));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
