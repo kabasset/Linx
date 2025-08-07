8b702732734bfdf3db7c66b214050dcf12171c1f
Antoine Basset <antoine.basset@cnes.fr>
2025-08-07 07:47:37 +0200

    Fix correlation tests

--- tests/ImageLinearFiltering_test.cpp
+++ tests/ImageLinearFiltering_test.cpp
@@ -20,26 +20,17 @@ BOOST_AUTO_TEST_CASE(correlation_impulse_response_test)
   in_on_host(0, 0) = 1;
   Kokkos::deep_copy(in.container(), in_on_host.container());
 
-  std::cout << in << std::endl;
-
   auto k = Linx::no_init<Kokkos::complex<double>>("kernel", 3, 3).generate_offsets(); // Unique values
   k += Kokkos::complex<double>(0, 1); // Non-null imaginary part
-
-  std::cout << k << std::endl;
-
   auto out = Linx::Correlation(k)(in);
-
-  std::cout << out << std::endl;
 
   auto test = Linx::default_init<Kokkos::complex<double>>("test", Linx::cube<2_D, 2>());
   BOOST_TEST((out.domain() == test.domain()));
   Linx::for_each(
       "test",
       k.domain(),
-      KOKKOS_LAMBDA(int i, int j) { test(2, 2) = Kokkos::conj(k(i, j)); });
+      KOKKOS_LAMBDA(int i, int j) { test(-i, -j) = Kokkos::conj(k(i, j)); });
   test -= out;
-
-  std::cout << test << std::endl;
 
   BOOST_TEST(test.contains_only(0));
 }
@@ -48,15 +39,8 @@ BOOST_AUTO_TEST_CASE(correlation_unit_response_test)
 {
   auto in = Linx::fill("unit", 1, 5, 5);
 
-  std::cout << in << std::endl;
-
   auto k = Linx::no_init<int>("kernel", 3, 3).generate_offsets();
-
-  std::cout << k << std::endl;
-
   auto out = Linx::Correlation(k)(in);
-
-  std::cout << out << std::endl;
 
   auto test = Linx::default_init<int>("test", 5, 5);
   BOOST_TEST((out.domain() == test.domain()));
@@ -66,8 +50,6 @@ BOOST_AUTO_TEST_CASE(correlation_unit_response_test)
       k.domain(),
       KOKKOS_LAMBDA(int i, int j) { test(2 - i, 2 - j) = sum; });
   test -= out;
-
-  std::cout << test << std::endl;
 
   BOOST_TEST(test.contains_only(0));
 }
