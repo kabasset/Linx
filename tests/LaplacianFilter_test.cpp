8c428dc0844825d8626f2f2490c6d99a62c194d4
Antoine Basset <antoine.basset@cnes.fr>
2025-08-11 11:03:26 +0200

    Fix LaplacianFilter_test

--- tests/LaplacianFilter_test.cpp
+++ tests/LaplacianFilter_test.cpp
@@ -3,6 +3,7 @@
 
 #define BOOST_TEST_MODULE LaplacianFilterTest
 
+#include "Linx/Data/Box.h"
 #include "Linx/Data/Image.h"
 #include "Linx/Run/ProgramContext.h"
 #include "Linx/Transforms/LinearFiltering.h"
@@ -15,23 +16,26 @@ BOOST_AUTO_TEST_CASE(box_test)
 {
   const auto filter = Linx::separable_laplacian<0, 1>();
   const auto box = Linx::bbox(filter.footprint());
-  BOOST_TEST((box == Linx::Box<2>({-1, -1}, {2, 2})));
+  BOOST_TEST((box == Linx::Box({-1, -1}, {2, 2})));
 }
 
 BOOST_AUTO_TEST_CASE(impulse_test)
 {
-  const auto in_h = Linx::Raster<int, 3>("in", 5, 5, 5);
-  in_h(2, 2, 2) = 1;
+  using namespace Linx::Literals;
+
+  auto domain = Linx::cube<3_D, 2>();
+  const auto in_h = Linx::Raster<int, decltype(domain)>("in", domain);
+  in_h.origin() = 1;
   const auto out = Linx::separable_laplacian<0, 1, 2>(-1)(Linx::on_device(in_h));
   static_assert(std::is_same_v<decltype(out)::execution_space, Kokkos::DefaultExecutionSpace>);
-  const auto expected_h = Linx::Raster<int, 3>("exp", 5, 5, 5);
-  expected_h(2, 2, 1) = -1;
-  expected_h(2, 1, 2) = -1;
-  expected_h(1, 2, 2) = -1;
-  expected_h(2, 2, 2) = 6;
-  expected_h(3, 2, 2) = -1;
-  expected_h(2, 3, 2) = -1;
-  expected_h(2, 2, 3) = -1;
+  const auto expected_h = Linx::Raster<int, decltype(domain)>("exp", domain);
+  expected_h(0, 0, -1) = -1;
+  expected_h(0, -1, 0) = -1;
+  expected_h(-1, 0, 0) = -1;
+  expected_h(0, 0, 0) = 6;
+  expected_h(1, 0, 0) = -1;
+  expected_h(0, 1, 0) = -1;
+  expected_h(0, 0, 1) = -1;
   auto expected = Linx::on_device(expected_h);
   BOOST_TEST((out == expected));
 }
